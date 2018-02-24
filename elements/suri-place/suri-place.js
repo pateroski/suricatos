dd859978d0e007b671a2266957556e93c1ef3a67
pateroski <p4ter0ski@gmail.com>
2018-02-24 10:33:12 +0100

    [feat] added styles file for each fragment separately

---
+++ elements/suri-place/suri-place.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  class SuriPlace extends Polymer.Element {
+    static get is() { return 'suri-place'; }
+  }
+
+  window.customElements.define(SuriPlace.is, SuriPlace);
+})();
