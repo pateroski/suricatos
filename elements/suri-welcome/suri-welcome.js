03f3a1897d62a8c3469247cb600025a37fb4c34d
pateroski <p4ter0ski@gmail.com>
2018-08-04 10:44:41 +0200

    [feat] added countdown for showing how much time for wedding day is in place

--- elements/suri-welcome/suri-welcome.js
+++ elements/suri-welcome/suri-welcome.js
@@ -3,6 +3,37 @@
 
   class SuriWelcome extends Polymer.Element {
     static get is() { return 'suri-welcome'; }
+
+    static get properties() {
+      return {
+
+      };
+    }
+
+    _setCountDown() {
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      let countDown = new Date('Oct 13, 2018 13:00:00').getTime();
+      setInterval(() => {
+
+        let now = new Date().getTime();
+        let distance = countDown - now;
+
+        this.$.days.innerText = Math.floor(distance / (day));
+        this.$.hours.innerText = Math.floor((distance % (day)) / (hour));
+        this.$.minutes.innerText = Math.floor((distance % (hour)) / (minute));
+        this.$.seconds.innerText = Math.floor((distance % (minute)) / second);
+
+      }, second);
+    }
+
+    ready() {
+      super.ready();
+      this._setCountDown();
+    }
   }
 
   window.customElements.define(SuriWelcome.is, SuriWelcome);
