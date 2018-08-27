7ceb89ca998f01c170a6a9401dd9e723bdcd3c0e
pateroski <p4ter0ski@gmail.com>
2018-08-27 19:38:41 +0200

    [feat] added basic scaffolding and functionality to suri-modal

--- elements/suri-modal/suri-modal.js
+++ elements/suri-modal/suri-modal.js
@@ -3,6 +3,64 @@
 
   class SuriModal extends Polymer.Element {
     static get is() { return 'suri-modal'; }
+
+    static get properties() {
+      return {
+        /**
+         * Path Source image
+         * @type {String}
+         */
+        image: {
+          type: String,
+          value: ''
+        },
+
+        /**
+         * Title message
+         * @type {String}
+         */
+        title: {
+          type: String,
+          value: ''
+        },
+
+        /**
+         * Description message
+         * @type {String}
+         */
+        description: {
+          type: String,
+          value: ''
+        },
+
+        /**
+         * Action suggestion message
+         * @type {String}
+         */
+        suggestion: {
+          type: String,
+          value: ''
+        }
+
+      };
+    }
+
+    /**
+     * Open modal action for showing the content
+     */
+    openDialog() {
+      this.$.dialog.open();
+      document.body.appendChild(this);
+    }
+
+    exitDialog() {
+      this.$.dialog.close();
+      this.remove();
+    }
+
+    cancelDialog() {
+      this.remove();
+    }
   }
 
   window.customElements.define(SuriModal.is, SuriModal);
