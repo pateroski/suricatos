eb19244ccae27da602270c35d1f7ba64607f5f12
pateroski <p4ter0ski@gmail.com>
2018-08-27 18:01:06 +0200

    [feat] added modal for showing info inside

---
+++ elements/suri-modal/suri-modal.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  class SuriModal extends Polymer.Element {
+    static get is() { return 'suri-modal'; }
+  }
+
+  window.customElements.define(SuriModal.is, SuriModal);
+})();
