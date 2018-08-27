917584afa38732803ba7bad9d1c80789d9082641
pateroski <p4ter0ski@gmail.com>
2018-08-27 19:37:06 +0200

    [feat] added suri-timeline for further use

--- elements/suri-roadmap/suri-roadmap.js
+++ elements/suri-roadmap/suri-roadmap.js
@@ -3,6 +3,10 @@
 
   class SuriRoadmap extends Polymer.Element {
     static get is() { return 'suri-roadmap'; }
+
+    _showHelpModal() {
+      this.$.helpModal.openDialog();
+    }
   }
 
   window.customElements.define(SuriRoadmap.is, SuriRoadmap);
